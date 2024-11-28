81561e079f937854ec35cc6fced95c7b2550039d
Santos Joao (7) <joao.santos.7@volvo.com>
2024-11-28 11:51:34 -0300

    created CardList

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,6 +1,10 @@
 import "./Card.css";
 
-type Props = {}
+interface Props {
+    companyName: string;
+    ticker: string;
+    price: number;
+}
 
 const Card = (props: Props) => {
     return (
@@ -10,8 +14,8 @@ const Card = (props: Props) => {
                 src="https://images.unsplash.com/photo-1672617195387-1a890be98e28?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
             <div className='details'>
-                <h2>AAPL</h2>
-                <p>$110</p>
+                <h2>{props.companyName} ({props.ticker})</h2>
+                <p>${props.price}</p>
             </div>
             <p className='info'>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, temporibus.
