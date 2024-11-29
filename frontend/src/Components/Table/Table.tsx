1c5dbd2ee8dff4e28e02681dda25a77d94608c9c
Santos Joao (7) <joao.santos.7@volvo.com>
2024-11-29 17:51:42 -0300

    Created Ratio component

--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -22,7 +22,7 @@ const Table = (props: Props) => {
             <tr key={company.cik}>
                 {
                     configs.map((val: any) => {
-                        return <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">{val.render(company)}</td>
+                        return <td className="p3">{val.render(company)}</td>
                     })
                 }
             </tr>
@@ -40,8 +40,8 @@ const Table = (props: Props) => {
 
     return (
         <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-            <table>
-                <thead className="min-w-full divide-y divide=gray-200 m-5">{renderedHeaders}</thead>
+            <table className="min-w-full divide-y divide-gray-200 m-5">
+                <thead className="bg-gray-50">{renderedHeaders}</thead>
                 <tbody>{renderedRows}</tbody>
             </table>
         </div>
--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -1,3 +1,4 @@
+import RatioList from "../../Components/RatioList/RatioList"
 import Table from "../../Components/Table/Table"
 
 type Props = {}
@@ -7,6 +8,7 @@ const DesignPage = (props: Props) => {
         <>
             <h1>FinShark Design Page</h1>
             <h2>This is FinShark's design page. This is where we will house various design aspects of the app</h2>
+            <RatioList />
             <Table />
         </>
     )
